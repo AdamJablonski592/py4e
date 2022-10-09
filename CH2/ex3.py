5e19c3bda2647147db220e44421852b3e8646a4f
1 <1>
2022-10-09 15:06:17 +0200

    Chapter 2 exercises

---
+++ CH2/ex3.py
@@ -0,0 +1,5 @@
+hours = float(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+
+pay = round(hours * rate, 2)
+print(f"Pay: {pay}")
---
+++ CH2/ex5.py
@@ -0,0 +1,4 @@
+celsius = float(input("Enter temperature in Celcius: "))
+
+fahrenheit = (celsius * (9/5)) + 32
+print(f"{celsius}°C equals to {fahrenheit}°F")
